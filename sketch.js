467b4eb2769a5033011acff2f3dbe5c84d34927e
yunella77 <413730374@o365.tku.edu.tw>
2025-05-02 10:24:56 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,15 @@ let capture;
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // 全螢幕畫布
-  background("#ECECFF"); // 背景顏色為黑色
+  background(0); // 背景顏色為黑色
   capture = createCapture(VIDEO); // 擷取攝影機影像
   capture.size(windowWidth * 0.8, windowHeight * 0.8); // 設定影像寬高為視窗大小的80%
   capture.hide(); // 隱藏原始的攝影機影像
 }
 
 function draw() {
-  background("#ECECFF"); // 確保背景保持黑色
-  image(capture, width * 0.1, height * 0.1, width * 0.8, height * 0.8); // 將影像顯示在視窗中間
+  background(0); // 確保背景保持黑色
+  translate(width, 0); // 將畫布的原點移到右上角
+  scale(-1, 1); // 水平翻轉畫布
+  image(capture, width * 0.1, height * 0.1, width * 0.8, height * 0.8); // 顯示翻轉後的影像
 }
